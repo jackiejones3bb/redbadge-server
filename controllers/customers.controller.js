a3f9b33a898b259675c309383d5e6e11dcf5e7f9
jackiejones3bb <jackiejones3bb@gmail.com>
2021-07-11 14:19:48 -0400

    coded programs and customers controller

--- controllers/customers.controller.js
+++ controllers/customers.controller.js
@@ -5,4 +5,40 @@ router.get('/test', (req, res) => {
     res.send('Testing from customers controller');
 })
 
+router.get('/', (req, res) => {
+    Customer.findAll()
+    .then((customers) => res.status(200).json(customers))
+    .catch((err) => res.status(500).json({ message:"No customers found", error:err }));
+})
+
+router.get('/:id', (req, res) => {
+    Customer.findAll({
+        where: {
+          id: req.params.id,
+        },
+      })
+    .then((customers) => {
+        const statusCode = customers.length === 0 ? 404: 200
+        res.status(statusCode).json(customers)})
+    .catch((err) => res.status(500).json({ message:"Customer not found", error:err }));
+})
+
+router.put('/:id', (req, res) => {
+    Customer.update({
+        phone: req.body.phone,
+        street: req.body.street,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip
+    },{
+        where: {
+          id: req.params.id,
+        },
+      })
+    .then((customers) => {
+        const statusCode = customers[0] === 0 ? 404: 200
+        res.status(statusCode).json(customers)})
+    .catch((err) => res.status(500).json({ message:"Error, profile not updated", error:err }));
+})
+
 module.exports = router;
--- controllers/programs.controller.js
+++ controllers/programs.controller.js
@@ -6,4 +6,67 @@ router.get('/test', (req, res) => {
     res.send('Testing from programs controller');
 })
 
+router.get('/', (req, res) => {
+    LoyaltyProgram.findAll()
+    .then((programs) => {
+        const statusCode = programs.length === 0 ? 404: 200
+        res.status(statusCode).json(programs)})
+    .catch((err) => res.status(500).json({ message:"No loyalty program found", error:err }));
+})
+
+router.get('/:id', (req, res) => {
+    LoyaltyProgram.findAll({
+        where: {
+          id: req.params.id,
+        },
+      })
+    .then((programs) => {
+        const statusCode = programs.length === 0 ? 404: 200
+        res.status(statusCode).json(programs)})
+    .catch((err) => res.status(500).json({ message:"No loyalty program found", error:err }));
+})
+
+router.put('/:id', (req, res) => {
+    LoyaltyProgram.update({
+        name: req.body.name,
+        numOfPunches: req.body.numOfPunches,
+        rewardType: req.body.rewardType,
+    },{
+        where: {
+          id: req.params.id,
+        },
+      })
+    .then((programs) => {
+        const statusCode = programs[0] === 0 ? 404: 200
+        res.status(statusCode).json(programs)})
+    .catch((err) => res.status(500).json({ message:"Error, loyalty program not updated", error:err }));
+})
+
+router.post('/', (req, res) => {
+    LoyaltyProgram.create({
+        name: req.body.name,
+        numOfPunches: req.body.numOfPunches,
+        rewardType: req.body.rewardType,
+        businessId: req.body.busId
+    })
+
+    .then((programs) => {
+        const statusCode = programs[0] === 0 ? 404: 200
+        res.status(statusCode).json(programs)})
+    .catch((err) => res.status(500).json({ message:"Error, loyalty program not created", error:err }));
+})
+
+router.delete('/:id', (req, res) => {
+    LoyaltyProgram.destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+    .then((programs) => {
+        const statusCode = programs.length === 0 ? 404: 200
+        res.status(statusCode).json(programs)})
+    .catch((err) => res.status(500).json({ message:"Loyalty program not deleted", error:err }));
+})
+
+
 module.exports = router;
